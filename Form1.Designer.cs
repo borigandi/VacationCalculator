7448f757861f6fbede79ef61dd676bdaae8cdad4
Bogdan <49912682+borigandi@users.noreply.github.com>
2020-01-20 21:17:56 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace VacationCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dateTimeStart = new System.Windows.Forms.DateTimePicker();
+            this.dateTimeEnd = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.resultTextBox = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dateTimeStart
+            // 
+            this.dateTimeStart.Font = new System.Drawing.Font("Lucida Bright", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimeStart.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimeStart.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.dateTimeStart.Location = new System.Drawing.Point(66, 39);
+            this.dateTimeStart.MaxDate = new System.DateTime(2028, 12, 31, 0, 0, 0, 0);
+            this.dateTimeStart.MinDate = new System.DateTime(2000, 1, 1, 0, 0, 0, 0);
+            this.dateTimeStart.Name = "dateTimeStart";
+            this.dateTimeStart.Size = new System.Drawing.Size(121, 22);
+            this.dateTimeStart.TabIndex = 0;
+            this.dateTimeStart.Value = new System.DateTime(2020, 1, 15, 0, 0, 0, 0);
+            this.dateTimeStart.ValueChanged += new System.EventHandler(this.dateTimeStart_ValueChanged);
+            this.dateTimeStart.KeyDown += new System.Windows.Forms.KeyEventHandler(this.dateTimeStart_PressEnter);
+            // 
+            // dateTimeEnd
+            // 
+            this.dateTimeEnd.Font = new System.Drawing.Font("Lucida Bright", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimeEnd.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimeEnd.Location = new System.Drawing.Point(66, 72);
+            this.dateTimeEnd.MaxDate = new System.DateTime(2028, 12, 31, 0, 0, 0, 0);
+            this.dateTimeEnd.MinDate = new System.DateTime(2000, 1, 1, 0, 0, 0, 0);
+            this.dateTimeEnd.Name = "dateTimeEnd";
+            this.dateTimeEnd.Size = new System.Drawing.Size(121, 22);
+            this.dateTimeEnd.TabIndex = 1;
+            this.dateTimeEnd.Value = new System.DateTime(2020, 1, 15, 0, 0, 0, 0);
+            this.dateTimeEnd.ValueChanged += new System.EventHandler(this.dateTimeEnd_ValueChanged);
+            this.dateTimeEnd.KeyDown += new System.Windows.Forms.KeyEventHandler(this.dateTimeEnd_PressEnter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "з:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "по:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(44, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "кіл-сть дн. відп.:";
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.BackColor = System.Drawing.Color.Green;
+            this.resultTextBox.ForeColor = System.Drawing.SystemColors.Info;
+            this.resultTextBox.Location = new System.Drawing.Point(145, 134);
+            this.resultTextBox.MaxLength = 3;
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.ReadOnly = true;
+            this.resultTextBox.Size = new System.Drawing.Size(42, 22);
+            this.resultTextBox.TabIndex = 7;
+            this.resultTextBox.Text = "0";
+            this.resultTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(147, 106);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(40, 22);
+            this.numericUpDown1.TabIndex = 9;
+            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            this.numericUpDown1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.numeric_PressEnter);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(76, 137);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 15);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "результат:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(233, 195);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.resultTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimeEnd);
+            this.Controls.Add(this.dateTimeStart);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Font = new System.Drawing.Font("Lucida Bright", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.Text = "Vacation 1.0b";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimeStart;
+        private System.Windows.Forms.DateTimePicker dateTimeEnd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        internal System.Windows.Forms.TextBox resultTextBox;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VacationCalculator
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<int, string> floatDateEaster = new Dictionary<int, string>();
+        private Dictionary<int, string> floatDateTrinity = new Dictionary<int, string>();
+
+        private List<string> blackoutDays = new List<string>() 
+        { "01/01/", "07/01/", "08/03/", "01/05/", "02/05/", "09/05/", "28/06/", "24/08/", "14/10/", "25/12/" };
+
+        public Form1()
+        {
+            DictionaryAddAsync();
+            InitializeComponent();
+        }
+
+        #region ControlFocus KeyDown
+
+        private void dateTimeStart_PressEnter(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                dateTimeEnd.Focus();
+        }
+
+        private void dateTimeEnd_PressEnter(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                numericUpDown1.Focus();
+        }
+
+        private void numeric_PressEnter(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                dateTimeStart.Focus();
+        }
+
+        #endregion
+
+        #region ValueChanged event
+
+        private async void dateTimeStart_ValueChanged(object sender, EventArgs e)
+        {
+            double result = await Task.Run(() => CalcResult());
+            resultTextBox.Text = result.ToString();
+        }
+
+        private async void dateTimeEnd_ValueChanged(object sender, EventArgs e)
+        {
+            double result = await Task.Run(() => CalcResult());
+            resultTextBox.Text = result.ToString();
+        }
+
+        private async void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            double result = await Task.Run(() => CalcResult());
+            resultTextBox.Text = result.ToString();
+        }
+
+        #endregion
+
+        private double CalcResult()
+        {
+            // Вираховує кількість святкових днів (т.з. blackoutDays) в обраному періоді
+            // Та за спец. формулою розраховує кількість днів відпустки, доступних працівнику в обраний період.
+
+            int blackoutDaysCount = 0; 
+            DateTime dateStart = dateTimeStart.Value.Date;
+            DateTime dateEnd = dateTimeEnd.Value.Date;
+            var daysCount = (dateEnd - dateStart).Days + 1;
+            double numberDays = (int)numericUpDown1.Value;
+
+            for (int year = dateStart.Year; year < dateEnd.Year + 1; year++)
+            {
+                foreach (string day in blackoutDays)
+                {
+                    DateTime dateToCheck = DateTime.Parse(day + year);
+
+                    if (dateToCheck.IsInRange(dateStart, dateEnd))
+                    {
+                        if (year < 2016 && day == "14/10/")
+                            continue;
+                        if (year < 2017 && day == "25/12/")
+                            continue;
+                        if (year >= 2018 && day == "02/05/")
+                            continue;
+
+                        blackoutDaysCount++;
+                    }
+                }
+            }
+
+            for (int year = dateStart.Year; year < dateEnd.Year + 1; year++)
+            {
+                DateTime dateToCheckEaster = DateTime.Parse(floatDateEaster[year] + year);
+                DateTime dateToCheckTrinity = DateTime.Parse(floatDateTrinity[year] + year);
+
+                if (dateToCheckEaster.IsInRange(dateStart, dateEnd))
+                    blackoutDaysCount++;
+                if (dateToCheckTrinity.IsInRange(dateStart, dateEnd))
+                    blackoutDaysCount++;
+            }
+
+            double result = numberDays / (365 - 11) * (daysCount - blackoutDaysCount);
+
+            return Math.Round(result, 2);
+        }
+
+        private async Task DictionaryAddAsync()
+        {
+            // Ініціалізація коллекцій свят з плаваючими датами (Пасха та Трійця)
+            // в заданому замовником діапазоні (з 2000 по 2028 роки включно)
+
+            await Task.Run(() =>
+            {
+                #region Trinity
+                Task.Run(() =>
+                {
+                    floatDateTrinity.Add(2000, "18/06/");
+                    floatDateTrinity.Add(2001, "03/06/");
+                    floatDateTrinity.Add(2002, "23/06/");
+                    floatDateTrinity.Add(2003, "15/06/");
+                    floatDateTrinity.Add(2004, "30/05/");
+                    floatDateTrinity.Add(2005, "19/06/");
+                    floatDateTrinity.Add(2006, "11/06/");
+                    floatDateTrinity.Add(2007, "27/05/");
+                    floatDateTrinity.Add(2008, "15/06/");
+                    floatDateTrinity.Add(2009, "07/06/");
+                    floatDateTrinity.Add(2010, "23/05/");
+                    floatDateTrinity.Add(2011, "12/06/");
+                    floatDateTrinity.Add(2012, "03/06/");
+                    floatDateTrinity.Add(2013, "23/06/");
+                    floatDateTrinity.Add(2014, "08/06/");
+                    floatDateTrinity.Add(2015, "31/05/");
+                    floatDateTrinity.Add(2016, "19/06/");
+                    floatDateTrinity.Add(2017, "04/06/");
+                    floatDateTrinity.Add(2018, "27/05/");
+                    floatDateTrinity.Add(2019, "16/06/");
+                    floatDateTrinity.Add(2020, "07/06/");
+                    floatDateTrinity.Add(2021, "20/06/");
+                    floatDateTrinity.Add(2022, "12/06/");
+                    floatDateTrinity.Add(2023, "04/06/");
+                    floatDateTrinity.Add(2024, "23/06/");
+                    floatDateTrinity.Add(2025, "08/06/");
+                    floatDateTrinity.Add(2026, "31/05/");
+                    floatDateTrinity.Add(2027, "20/06/");
+                    floatDateTrinity.Add(2028, "04/06/");
+                });
+                #endregion
+                #region Easter
+                floatDateEaster.Add(2000, "30/04/");
+                floatDateEaster.Add(2001, "15/04/");
+                floatDateEaster.Add(2002, "05/05/");
+                floatDateEaster.Add(2003, "27/04/");
+                floatDateEaster.Add(2004, "11/04/");
+                floatDateEaster.Add(2005, "01/05/");
+                floatDateEaster.Add(2006, "23/04/");
+                floatDateEaster.Add(2007, "08/04/");
+                floatDateEaster.Add(2008, "27/04/");
+                floatDateEaster.Add(2009, "19/04/");
+                floatDateEaster.Add(2010, "04/04/");
+                floatDateEaster.Add(2011, "24/04/");
+                floatDateEaster.Add(2012, "15/04/");
+                floatDateEaster.Add(2013, "05/05/");
+                floatDateEaster.Add(2014, "20/04/");
+                floatDateEaster.Add(2015, "12/04/");
+                floatDateEaster.Add(2016, "01/05/");
+                floatDateEaster.Add(2017, "16/04/");
+                floatDateEaster.Add(2018, "08/04/");
+                floatDateEaster.Add(2019, "28/04/");
+                floatDateEaster.Add(2020, "19/04/");
+                floatDateEaster.Add(2021, "02/05/");
+                floatDateEaster.Add(2022, "24/04/");
+                floatDateEaster.Add(2023, "16/04/");
+                floatDateEaster.Add(2024, "05/05/");
+                floatDateEaster.Add(2025, "20/04/");
+                floatDateEaster.Add(2026, "12/04/");
+                floatDateEaster.Add(2027, "02/05/");
+                floatDateEaster.Add(2028, "16/04/");
+                #endregion
+            });
+        }
+    }
+
+    static class DateTimeExtension
+    {
+        // Перевіряє чи входить дата, на якій викликано метод, в діапазон beginDate-endDate включно
+
+        public static bool IsInRange(this DateTime currentDate, DateTime beginDate, DateTime endDate)
+        {
+            return (currentDate >= beginDate && currentDate <= endDate);
+        }
+    }
+}
